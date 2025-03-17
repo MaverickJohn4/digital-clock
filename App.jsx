cba942bd4ff8d3fb93d330ff4a9d648a1ca9821d
MaverickJohn4 <maverickjohn28@gmail.com>
2025-03-17 09:35:38 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import LiveClock from './Component/LiveClock';
+
+const App = () => {
+  return (
+    <div>
+      <LiveClock />
+    </div>
+  );
+};
+
+export default App;
